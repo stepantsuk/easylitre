cf676dab87e8bdff59095f11b237fbf5cd394dc5
stepantsuk <stepan.tsukanov@yandex.ru>
2023-12-07 18:10:32 +0300

    container width

--- src/ui-kit/commonStyles.ts
+++ src/ui-kit/commonStyles.ts
@@ -6,7 +6,7 @@ import {
 } from './sharedStyles'
 
 export const Container = styled.div`
-  width: 460px;
+  width: 459px;
   margin: 0 auto;
   
   @media screen and (max-width: ${mobileBreakpoint}) {
